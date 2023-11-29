69c267cfa46f0717e3dbce4dfb9bcec5570a5ecd
hovanhung1506 <hungho269@gmail.com>
2023-11-30 03:48:05 +0700

    add, remove by ajax

--- src/main/java/com/example/shoppingcart/controller/CartController.java
+++ src/main/java/com/example/shoppingcart/controller/CartController.java
@@ -3,18 +3,15 @@ package com.example.shoppingcart.controller;
 import com.example.shoppingcart.domain.Cart;
 import com.example.shoppingcart.domain.Product;
 import com.example.shoppingcart.entity.CartEntity;
+import com.example.shoppingcart.entity.OrderDetailsEntity;
 import com.example.shoppingcart.service.CartService;
-import com.example.shoppingcart.service.ProductService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("cart")
@@ -22,9 +19,6 @@ public class CartController {
 
     @Autowired
     private CartService cartService;
-
-    @Autowired
-    private ModelMapper modelMapper;
 
     @Autowired
     private CartEntity cartEntity;
@@ -47,5 +41,35 @@ public class CartController {
     public String removeItem(@ModelAttribute Product product) {
         cartService.removeItem(product);
         return "redirect:/cart";
+    }
+
+    @PostMapping("/change-quantity")
+    @ResponseBody
+    public ResponseEntity<?> changeQuantity(@RequestParam String action, @RequestParam Long id) {
+        Map<String, Object> response = new HashMap<>();
+        Product product = new Product();
+        product.setId(id);
+
+        switch (action) {
+            case "increment" -> cartService.addItem(product);
+            case "decrement" -> cartService.removeItem(product);
+            default -> {
+                response.put("message", "method has not been defined");
+                return ResponseEntity.ok(response);
+            }
+        }
+        int quantity = Optional.of(cartEntity.getAll())
+                .orElse(new ArrayList<>())
+                .stream()
+                .filter(details -> Objects.equals(details.getProductEntity().getId(), product.getId()))
+                .findFirst()
+                .orElse(new OrderDetailsEntity())
+                .getQuantity();
+
+        response.put("numberOfItems", cartEntity.count());
+        response.put("quantity", quantity);
+        response.put("message", "success");
+
+        return ResponseEntity.ok(response);
     }
 }
